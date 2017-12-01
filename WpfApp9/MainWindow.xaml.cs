169f9d7b9643e26a9f9a9bd3d8d92675f10cc19c
Dante Budin <dante.budin@immalle.eu>
2017-12-01 11:04:34 +0100

    regen

--- WpfApp9/MainWindow.xaml.cs
+++ WpfApp9/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp9
 {
@@ -20,9 +21,66 @@ namespace WpfApp9
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Random randomNumber = new Random();
+
+        private double x, y, size;
+        private SolidColorBrush brush;
+        
+        private DispatcherTimer timer = new DispatcherTimer();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            gapLabel.Content = Convert.ToString(gapSlider.Value);
+            brush = new SolidColorBrush(Colors.LightBlue);
+            timer.Interval = TimeSpan.FromMilliseconds(gapSlider.Value);
+            timer.Tick += timer_Tick;
         }
+
+
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+
+        }
+        private void stopButoon_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            paperCanvas.Children.Clear();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            x = randomNumber.Next(0, Convert.ToInt32(paperCanvas.Width));
+            y = randomNumber.Next(0, Convert.ToInt32(paperCanvas.Height));
+            size = randomNumber.Next(1, 40);
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = size;
+            ellipse.Height = size;
+            ellipse.Stroke = brush;
+            ellipse.Fill = brush;
+            ellipse.Margin = new Thickness(x, y, 0, 0);
+            paperCanvas.Children.Add(ellipse);
+
+
+            timer.Stop();
+            int ms = randomNumber.Next(1, Convert.ToInt32(gapSlider.Value));
+            timer.Interval = TimeSpan.FromMilliseconds(ms);
+            timer.Start();
+
+        }
+        private void gapSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int timeGap = Convert.ToInt32(gapSlider.Value);
+            gapLabel.Content = Convert.ToString(timeGap);
+        }
+
+
     }
 }
